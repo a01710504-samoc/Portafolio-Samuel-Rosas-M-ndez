6e88446951c7611dac35687d285a9df0d904b8b3
a01710504-samoc <a01710504@tec.mx>
2025-09-14 08:42:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
       setTimeout(() => {
         currentAudio.pause();
         currentAudio.currentTime = 0;
-      }, 22000);
+      }, 222000);
     });
   }
 
